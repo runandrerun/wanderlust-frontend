2892f78d378cab753888b7e03ac46dcc02cad49e
runandrerun <the.asantiagojr@gmail.com>
2019-03-09 16:28:22 -0500

    Custom MUI styling component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './themes/materialStyling.js';
 
 class App extends Component {
   render() {
---
+++ src/themes/materialStyling.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  typography: {
+    fontFamily: "'Montserrat', sans-serif",
+    fontSize:14,
+    textTransform: "none",
+    color: "#484848",
+  },
+  palette: {
+    primary: {
+      light: "#ff8e8c",
+      main: "#484848",
+      dark: "#c62035",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#4da9b7",
+      main: "#ff5a5f",
+      dark: "#004e5a",
+      contrastText: "#000"
+    }
+  },
+});
