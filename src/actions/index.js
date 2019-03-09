381e778735bf5b06c4b687d0d92ba14a6b152566
runandrerun <the.asantiagojr@gmail.com>
2019-03-09 15:49:21 -0500

    Added Adapters and Actions

---
+++ src/actions/index.js
@@ -0,0 +1,174 @@
+import { fetchDog, fetchDogs, fetchUpdateDog, createDog } from '../adapters/dogsAdapter';
+
+export const selectUser = (user) => {
+  return {
+    type: 'SELECT_USER',
+    payload: {user}
+  }
+}
+
+export const loadUsers = () => {
+  return (dispatch) => {
+    fetchUsers()
+      .then(users => {
+        dispatch(setUsers(users))
+      })
+  }
+}
+
+export const loadUser = (user) => {
+  return (dispatch) => {
+    fetchUser(user)
+      .then(userRes => {
+        dispatch(currentUser(userRes))
+      })
+  }
+}
+
+export const editUser = (user) => {
+  return (dispatch) => {
+    fetchUpdateUser(user)
+      .then(userRes => {
+        dispatch(setUser(userRes))
+      })
+  }
+}
+
+export const logUser = (userData) => {
+  return (dispatch) => {
+    return loginUser(userData)
+    .then(userRes => {
+      dispatch(currentUser(userRes))
+      // console.log('Inside of logUser in index.js', userRes)
+      localStorage.setItem('token', userRes.jwt)
+      return userRes
+    })
+  }
+}
+
+export const newUser = (userData) => {
+  return (dispatch) => {
+    createUser(userData)
+    .then(userRes => {
+      console.log('Inside newUser', userRes)
+
+      localStorage.setItem('token', userRes.jwt)
+    })
+  }
+}
+
+export const logout = () => {
+  localStorage.clear()
+  return (dispatch) => {
+    dispatch(logoutCurrentUser())
+  }
+}
+
+export const auth = (token) => {
+  return (dispatch) => {
+    reAuth(token)
+    .then(res => {
+      console.log('Inside Actions', res)
+      dispatch(authenticateUser(res))
+    })
+  }
+}
+
+export const selectedDogPark = (dogPark) => {
+  return {
+    type: 'SELECT_DOG_PARK',
+    payload: {dogPark}
+  }
+}
+
+export const loadDogParks = () => {
+  return (dispatch) => {
+    fetchDogParks()
+      .then(dogParks => {
+        dispatch(setDogParks(dogParks))
+      })
+  }
+}
+
+export const loadDogPark = (dogPark) => {
+  return (dispatch) => {
+    fetchDogPark(dogPark)
+      .then(dogParkRes => {
+        dispatch(setDogPark(dogParkRes))
+      })
+  }
+}
+
+////////
+
+const setDogParks = (dogParks) => {
+  return {
+    type: 'LOAD_DOG_PARKS',
+    payload: {
+      dogParks
+    }
+  }
+}
+
+const setDogPark = (dogPark) => {
+  return {
+    type: 'SELECT_DOG_PARK',
+    payload: {
+      dogPark
+    }
+  }
+}
+
+const authenticateUser = (user) => {
+  return {
+    type: 'AUTH_USER',
+    payload: {
+      user
+    }
+  }
+}
+
+const logoutCurrentUser = () => {
+  return {
+    type: 'LOGOUT_USER',
+    payload: {
+      user: {}
+    }
+  }
+}
+
+const setUsers = (users) => {
+  return {
+    type: 'LOAD_USERS',
+    payload: {
+      users
+    }
+  }
+}
+
+const setUser = (user) => {
+  return {
+    type: 'EDIT_USER',
+    payload: {
+      user
+    }
+  }
+}
+
+const currentUser = (user) => {
+  return {
+    type: 'LOGIN_USER',
+    payload: {
+      user
+    }
+  }
+}
+
+const registerUser = (user) => {
+  return {
+    type: 'REGISTER_USER',
+    payload: {
+      user
+    }
+  }
+}
---
+++ src/adapters/usersAdapter.js
@@ -0,0 +1,66 @@
+export const fetchUpdateUser = (user) => {
+  const url = `http://localhost:3000/users/${user.id}`
+  const options = {
+    method: 'PATCH',
+    mode: 'cors',
+    headers: {
+      Accepts: 'application/json',
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({user: user})
+  }
+  return fetch(url, options)
+  .then(res => res.json())
+};
+
+export const fetchUsers = () => {
+  return fetch(`http://localhost:3000/users`)
+  .then(res => res.json())
+};
+
+export const fetchUser = (user) => {
+  return fetch(`http://localhost:3000/users/${user}`)
+  .then(res => res.json())
+};
+
+export const createUser = (user) => {
+  const url = `http://localhost:3000/users`
+  console.log('User Adapter', user)
+  const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      Accepts: 'application/json',
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({user: user})
+  }
+  return fetch(url, options)
+  .then(res => res.json())
+};
+
+export const loginUser = (userData) => {
+  return fetch(`http://localhost:3000/login`, {
+    method: 'POST',
+    body: JSON.stringify({user: {username: userData.username, password: userData.password}}),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+  .then(res => res.json())
+};
+
+export const reAuth = (token) => {
+  const options = {
+        headers : {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: token
+        }
+      }
+  return fetch('http://localhost:3000/reauth', options)
+    .then(resp => resp.json())
+};
