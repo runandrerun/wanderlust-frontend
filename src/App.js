df553488980798cbb50f38bcc19f4b8c84882e04
runandrerun <the.asantiagojr@gmail.com>
2019-03-09 20:01:50 -0500

    Implemented MUI Theme Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import NavContainer from './containers/NavContainer.js';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './App.css';
 import './themes/materialStyling.js';
 
@@ -8,7 +9,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MuiThemeProvider theme={theme}>
         <NavContainer />
+        // <SubtitleSection />
+        // <LocationsGrid locations={this.state.locations} />
+        </MuiThemeProvider>
       </div>
     );
   }
